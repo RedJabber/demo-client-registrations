25060772798cb3149fbbd600f56df9016f481dff
RedJabber <Andrew.Arefyev@gmail.com>
2023-01-23 23:37:45 +0300

    initial project preparation

--- src/test/java/com/panda/sputnik/demo/TestApp.java
+++ src/test/java/com/panda/sputnik/demo/TestApp.java
@@ -2,18 +2,22 @@ package com.panda.sputnik.demo;
 
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @Slf4j
 @ActiveProfiles("test")
 @SpringBootTest(
-        // classes = App.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        properties = {
-//                "application.porscheUser.jwt.issuer=http://login.localhost"
-                "MY_HOST=super-duper-host"
-        })
+        classes = Application.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestApp {
     
+
+    @Test
+    void testAppLoaded() {
+
+    }
+
 }
